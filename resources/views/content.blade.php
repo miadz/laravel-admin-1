d4ef886fb1fa7b48763f28dfdcbbfd82cea466ef
miadz <miada48@gmail.com>
2018-10-29 03:57:27 +0330

    fix 2 -  breadcrumb position and add home translate to breadcrumb
    fix 2 -  breadcrumb position and add home translate to breadcrumb

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -32,7 +32,7 @@
                 @endforeach
             </ol>
         @elseif(config('admin.enable_default_breadcrumb'))
-            <ol class="breadcrumb" style="margin-right: 30px;">
+            <ol class="breadcrumb" style="position: relative;margin-bottom: 10px;">
                 <li><a href="{{ admin_url('/') }}"><i class="fa fa-dashboard"></i>  {{ trans('admin.home') }}</a></li>
                 @for($i = 2; $i <= count(Request::segments()); $i++)
                     <li>
