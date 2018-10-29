a65187ed6d4f0c722d7566b66a47b37f23d51cc2
miadz <miada48@gmail.com>
2018-10-29 03:44:30 +0330

    added 'home' trans to lang fa - used for breadcrumb
    added 'home' trans to lang fa - used for breadcrumb

--- resources/lang/fa/admin.php
+++ resources/lang/fa/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'home'                     => 'خانه',
     'online'                => 'آنلاین',
     'login'                 => 'ورود',
     'logout'                => 'خروج',
